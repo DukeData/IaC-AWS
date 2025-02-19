e6a4bc954f1f8576e44b18977ea9fdb22ea118d5
PuppySQL <dukedatasolutions@gmail.com>
2025-02-19 15:21:07 -0500

    Rds Hello

--- main.tf
+++ main.tf
@@ -71,7 +71,6 @@ module "ec2_instance" {
     Environment = "dev"
   }
 }
-
 # Creating RDS SQL Instance
 # Default provider configuration
 resource "aws_db_instance" "default" {
@@ -80,7 +79,7 @@ resource "aws_db_instance" "default" {
   engine               = "sqlserver-se"
   engine_version       = "14.00.3035.2.v1"
   instance_class       = "db.t3.medium"
-  identifier           = "w2d-rdssql01"
+  identifier           = ""
   username             = var.db_username
   password             = var.db_password
   publicly_accessible  = false
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+db_username = "adminsql"
+db_password = "Password1234"
