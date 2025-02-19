f6d42ff7fe833641c96173830f28aa112b2c92d8
PuppySQL <dukedatasolutions@gmail.com>
2025-02-19 10:35:45 -0500

    Rds new

--- main.tf
+++ main.tf
@@ -91,5 +91,13 @@ resource "aws_db_instance" "default" {
     Environment = "dev"
   }
 }
+variable "db_username" {
+  type        = string
+  description = "The username for the RDS instance"
+}
 
-
+variable "db_password" {
+  type        = string
+  description = "The password for the RDS instance"
+  sensitive   = true
+}
