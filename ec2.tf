a85b790fea13a04b8d7cc0b3021f6a044e9f8f00
PuppySQL <dukedatasolutions@gmail.com>
2025-02-26 19:32:36 -0500

    Adding new now

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
 # Creating EC2 instance
 module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  name = "w2d-server02"
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  name                   = "w2d-server02"
   ami                    = "ami-0c55b159cbfafe1f0"
   instance_type          = "t2.micro"
-  #key_name               = "user1" - Uncomment this line to use your own key pair
+  #key_name              = "user1" - Uncomment this line to use your own key pair
   monitoring             = true
   #vpc_security_group_ids = ["sg-12345678"]
   #subnet_id              = "subnet-eddcdzz4"
@@ -14,4 +14,25 @@ module "ec2_instance" {
     Terraform   = "true"
     Environment = "dev"
   }
+}
+# Creating EC2 instance
+module "ec2_instance" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  name                   = "w2d-server01"
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  #key_name              = "user1" - Uncomment this line to use your own key pair
+  monitoring             = true
+  #vpc_security_group_ids = ["sg-12345678"]
+  #subnet_id              = "subnet-eddcdzz4"
+  version = "4.0.0" 
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
 }
